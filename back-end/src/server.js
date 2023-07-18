1b0f472c2de2822b2267d43c5fb9c2bfd619d9bb
Britt-H <brittain.hoover@gmail.com>
2023-07-18 16:06:25 -0400

    testing server issue resolution

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,30 @@
+// const { PORT = 5001 } = process.env;
+
+// const app = require("./app");
+// const knex = require("./db/connection");
+
+// knex.migrate
+//   .latest()
+//   .then((migrations) => {
+//     console.log("migrations", migrations);
+//     app.listen(PORT, listener);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//     knex.destroy();
+//   });
+
+// function listener() {
+//   console.log(`Listening on Port ${PORT}!`);
+// }
+
+
 const { PORT = 5001 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
   .latest()
@@ -9,11 +32,4 @@ knex.migrate
     console.log("migrations", migrations);
     app.listen(PORT, listener);
   })
-  .catch((error) => {
-    console.error(error);
-    knex.destroy();
-  });
-
-function listener() {
-  console.log(`Listening on Port ${PORT}!`);
-}
+  .catch(console.error);
