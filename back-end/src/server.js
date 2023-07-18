879ab1b801ec030dad0b9922ef2de3b880775c68
Britt-H <brittain.hoover@gmail.com>
2023-07-18 16:08:15 -0400

    rollback to previous settings

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,30 +1,7 @@
-// const { PORT = 5001 } = process.env;
-
-// const app = require("./app");
-// const knex = require("./db/connection");
-
-// knex.migrate
-//   .latest()
-//   .then((migrations) => {
-//     console.log("migrations", migrations);
-//     app.listen(PORT, listener);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//     knex.destroy();
-//   });
-
-// function listener() {
-//   console.log(`Listening on Port ${PORT}!`);
-// }
-
-
 const { PORT = 5001 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
-
-const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
   .latest()
@@ -32,4 +9,11 @@ knex.migrate
     console.log("migrations", migrations);
     app.listen(PORT, listener);
   })
-  .catch(console.error);
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
+
+function listener() {
+  console.log(`Listening on Port ${PORT}!`);
+}
